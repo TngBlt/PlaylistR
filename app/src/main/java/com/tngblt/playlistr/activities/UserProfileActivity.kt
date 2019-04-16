ca606f55fdfb9333353dd8c5f4f685f73474abd3
Tanguy <tanguy.blochet@viacesi.fr>
2019-04-16 14:01:08 +0200

    UserActivity init

---
+++ app/src/main/java/com/tngblt/playlistr/activities/UserProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.tngblt.playlistr.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tngblt.playlistr.R
+
+class UserProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+
+
+}
